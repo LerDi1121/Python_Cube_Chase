7858665ac0684bb6c041739b1caa3de3c69727ed
vujadinjanjic <57356499+vujadinjanjic@users.noreply.github.com>
2019-12-06 11:34:49 +0100

    Dodat player klasa

--- Player.py
+++ Player.py
@@ -1,1 +1,44 @@
 """vujadin"""
+from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication, QWidget, QHBoxLayout, QLabel
+from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
+from PyQt5.QtGui import *
+
+class Player(QFrame):
+        PlayerWidth = 40
+        PlayerHeight = 40
+        pX = 0
+        pY = 0
+
+        LabelPlayer = 0
+        Picture = ""
+
+        def __init__(self, parent, x, y, picture):
+            super(parent,self).__init__()
+
+            self.initPlayer(parent, x, y, picture)
+
+        def initPlayer(self, parent, x, y, picture):
+            self.resize(800, 600)
+            self.pX = x
+            self.pY = y
+            self.Picture = picture
+
+            self.LabelPlayer = QLabel(parent)
+
+            PixmapPlayer = QPixmap(picture)
+            PixmapResizedPlayer = PixmapPlayer.scaled(self.PlayerWidth,self.PlayerHeight)
+
+            self.LabelPlayer.setPixmap(PixmapResizedPlayer)
+            self.LabelPlayer.move(x, y)
+
+        def updatePosition(self, x, y):
+            self.pX = x
+            self.pY = y
+
+            self.LabelPlayer(x, y)
+
+
+
+
+
+
